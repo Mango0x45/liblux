ccc88f0d865189821ad7f67ecfac4feec354e607
Thomas Voss <thomasvoss@live.com>
2022-08-09 12:32:49 +0200

    Add source code files in their uncompleted state
    The current state of the library contains support for the following
    functions:
     - int luxinit(struct luxdisp *)     | Init a luxdisp struct
     - int luxget(struct luxdisp *)      | Get the current brightness
     - int luxset(struct luxdisp *, int) | Set the current brightness
     - int luxmax(struct luxdisp *)      | Get the maximum brightness
     - void luxfree(struct luxdisp *)    | Free a luxdisp struct

---
+++ lux.h
@@ -0,0 +1,36 @@
+/*
+ * BSD Zero Clause License
+ *
+ * Copyright (c) 2022 Thomas Voss
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef LUX_H
+#define LUX_H
+
+#include <stdio.h>
+
+#define BACKLIGHT_DIR "/sys/class/backlight"
+
+struct luxdisp {
+	int __dirfd, __max;
+	FILE *__bstream;
+};
+
+int luxinit(struct luxdisp *);
+int luxget(struct luxdisp *);
+int luxset(struct luxdisp *, int);
+int luxmax(struct luxdisp *);
+void luxfree(struct luxdisp *);
+
+#endif /* !LUX_H */
